a57211eb34d53a068e7df1742514ead12ea0a4fc
Roberto Dantas <7robertodantas@gmail.com>
2016-12-07 18:15:24 -0300

    ScenarioBuilder em testes GerenciadorClientes
    Signed-off-by: Roberto Dantas <7robertodantas@gmail.com>

---
+++ src/test/java/controle/GerenciadorClientesScenarioBuilder.java
@@ -0,0 +1,131 @@
+package controle;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import dominio.Cliente;
+import excecao.ClienteInvalidoException;
+import excecao.DataException;
+import instanciahotel.ClienteHotel;
+
+public class GerenciadorClientesScenarioBuilder {
+	
+	private Cliente cliente;
+	private GerenciadorClientes gerenciador;
+	
+
+	public GerenciadorClientesScenarioBuilder(GerenciadorClientes gerenciador) {
+		super();
+		this.gerenciador = gerenciador;
+	}
+
+	public GerenciadorClientesScenarioBuilder criarClienteValido(){
+		// TODO Pegar último indice e incrementar - possivelmente mudar nome para criarNovoClienteValido
+		cliente = createClienteHotel(1L, "Roberto", "12345678900", "123123", "Rua importante", 1, 1, 1998);
+		assertTrue("Cliente não deve ser nulo", cliente != null);
+		assertTrue("Cliente deve ser válido", cliente.valido());	
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder criarClienteInvalido(){
+		cliente = createClienteHotel(1L, "", "", "", "Rua ", 1, 1, 2016);
+		assertTrue("Cliente não deve ser nulo", cliente != null);
+		assertTrue("Cliente deve ser inválido", !cliente.valido());	
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder cadastrarCliente() throws DataException, ClienteInvalidoException{
+		gerenciador.cadastrarCliente(cliente);
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder atualizarCliente() throws DataException, ClienteInvalidoException{
+		gerenciador.updateCliente(cliente);
+		return this;
+	}	
+	
+	public GerenciadorClientesScenarioBuilder removerCliente() throws DataException{
+		gerenciador.removerCliente(cliente);
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder setCodigoInvalido(){
+		if(cliente != null){
+			cliente.setCodigo(0L);
+		}
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder setNomeCliente(String nome){
+		if(cliente != null){
+			cliente.setNome(nome);
+		}
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder setCodigo(Long codigo){
+		if(cliente != null){
+			cliente.setCodigo(codigo);
+		}
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder tornarClienteInvalido(){
+		if(cliente != null){
+			cliente.setNome("");
+		}
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder tornarCodigoInvalido(){
+		if(cliente != null){
+			cliente.setCodigo(0L);
+		}
+		return this;
+	}	
+	
+	public GerenciadorClientesScenarioBuilder getCliente() throws DataException{
+		cliente = gerenciador.getCliente(cliente.getCodigo());
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder getCliente(Long codigo) throws DataException{
+		cliente = gerenciador.getCliente(codigo);
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder assertExists(){
+		assertTrue("Cliente deve estar cadastrado", gerenciador.exists(cliente.getCodigo()));
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder assertNotExists(){
+		assertTrue("Cliente não deve estar cadastrado", !gerenciador.exists(cliente.getCodigo()));
+		return this;
+	}
+	
+	public GerenciadorClientesScenarioBuilder assertCodigoValido(){
+		assertTrue("Id do cliente deve ser maior que 0", cliente.getCodigo() > 0);
+		return this;
+	}
+	
+	public Cliente getClienteInstance(){
+		return cliente;
+	}
+	
+	
+	private Cliente createClienteHotel(long codigo, String nome, String cpf, String rg, String endereco, int dia, int mes, int ano) {
+		Calendar dataNascimento = Calendar.getInstance();
+		dataNascimento.set(Calendar.DAY_OF_MONTH, dia);
+		dataNascimento.set(Calendar.MONTH, mes-1);
+		dataNascimento.set(Calendar.YEAR, ano);
+		return createClienteHotel(codigo, nome, cpf, rg, endereco, dataNascimento.getTime());
+	}
+	
+	private Cliente createClienteHotel(long codigo, String nome, String cpf, String rg, String endereco, Date nascimento) {
+		return new ClienteHotel(codigo, nome, cpf, rg, endereco, nascimento);
+	}
+
+}
---
+++ src/test/java/controle/GerenciadorClientesTest.java
@@ -0,0 +1,144 @@
+package controle;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import dao.DaoClienteMemoria;
+import dominio.Cliente;
+import excecao.ClienteInvalidoException;
+import excecao.DataException;
+
+
+public class GerenciadorClientesTest {
+	
+	private static GerenciadorClientes gerenciador;
+	private GerenciadorClientesScenarioBuilder builder;
+	
+	@BeforeClass
+	public static void beforeClass(){
+		gerenciador = new GerenciadorClientes();
+	}
+	
+	@Before
+	public void before(){
+		builder = new GerenciadorClientesScenarioBuilder(gerenciador);
+	}
+		
+	@After 
+	public void tearDown(){
+		DaoClienteMemoria.getInstance().clear();
+	}
+	
+	@Test(expected=ClienteInvalidoException.class)
+	public void testPreconditionInvalid() throws DataException, ClienteInvalidoException {
+		builder.criarClienteInvalido().cadastrarCliente();
+	}
+	
+	@Test
+	public void testCadastrarClienteNormalBehavior() throws DataException, ClienteInvalidoException {		
+		builder.criarClienteValido().cadastrarCliente().assertExists();
+	}
+	
+	@Test(expected=DataException.class)
+	public void testCadastrarClienteRepetidoExceptionalBehavior() throws DataException, ClienteInvalidoException{		
+		builder.criarClienteValido().cadastrarCliente().assertExists().cadastrarCliente();
+	}
+	
+	@Test
+	public void testRemoverClienteNormalBehavior() throws DataException, ClienteInvalidoException {				
+		builder.criarClienteValido().cadastrarCliente().assertExists().removerCliente().assertNotExists();
+	}
+
+	@Test(expected=DataException.class)
+	public void testRemoverClienteInexistenteExceptionalBehavior() throws DataException {				
+		builder.criarClienteValido().assertNotExists().removerCliente();
+	}
+	
+	@Test(expected=DataException.class)
+	public void testRemoverClienteIdInvalidoExceptionalBehavior() throws DataException {				
+		//Id <= 0 verifica uma condição especial da verificação e do exists()
+		builder.criarClienteValido().setCodigoInvalido().assertNotExists().removerCliente();
+	}
+
+	@Test
+	public void testAtualizarClienteNormalBehavior() throws DataException, ClienteInvalidoException{		
+		builder.criarClienteValido().cadastrarCliente().assertExists().setNomeCliente("Maria").atualizarCliente();
+	}
+	
+	@Test(expected=ClienteInvalidoException.class)
+	public void testAtualizarClienteInvalidoExceptionalBehavior() throws DataException, ClienteInvalidoException{	
+		builder.criarClienteValido().cadastrarCliente().assertExists().tornarClienteInvalido().atualizarCliente();
+	}
+	
+	@Test(expected=DataException.class)
+	public void testAtualizarClienteNaoCadastradoExceptionalBehavior() throws DataException, ClienteInvalidoException{
+		builder.criarClienteValido().assertNotExists().atualizarCliente();
+	}
+	
+	/** TESTES GET */
+	
+	@Test
+	public void testGetClienteNormalBehavior() throws DataException, ClienteInvalidoException {			
+		builder.criarClienteValido().assertCodigoValido().cadastrarCliente().assertExists();
+		
+		Cliente clienteCadastrado = builder.getClienteInstance();
+		
+		Cliente clienteBuscado = builder.getCliente(clienteCadastrado.getCodigo()).getClienteInstance();
+		
+		assertTrue("Cliente não deve ser nulo", clienteBuscado != null);
+		assertTrue("Cliente deve ter Id igual ao consultado", clienteCadastrado.getCodigo() == clienteBuscado.getCodigo());
+		
+	}
+	
+	@Test(expected=DataException.class)
+	public void testGetClienteInexistenteExceptionalBehavior() throws DataException {	
+		builder.criarClienteValido().assertCodigoValido().assertNotExists().getCliente(builder.getClienteInstance().getCodigo());
+	}
+	
+	@Test(expected=DataException.class)
+	public void testGetClienteIdInvalidoExceptionalBehavior() throws DataException {
+		//Id <= 0 verifica uma condição especial da verificação e do exists()
+		builder.criarClienteValido().tornarCodigoInvalido().assertNotExists().getCliente();
+	}
+	
+	/** TESTES LISTAR */
+	
+	public void testListarClientesListaVaziaNormalBehavior() throws DataException {		
+		List<Cliente> clientes = gerenciador.listarClientes();
+		assertFalse("Lista não deve ser nula", clientes == null);
+		assertTrue("Lista deve estar vazia", clientes.isEmpty());
+	}
+	
+	public void testListarClientesNormalBehavior() throws DataException, ClienteInvalidoException {	
+		
+		builder.criarClienteValido().assertNotExists().cadastrarCliente().assertExists();
+		
+		Cliente clienteCadastrado = builder.getClienteInstance();
+		
+		List<Cliente> clientes = gerenciador.listarClientes();
+		assertFalse("Lista não deve ser nula", clientes == null);
+		assertFalse("Lista não deve ser vazia", clientes.isEmpty());
+		assertTrue("Lista deve ter 1 item", clientes.size() == 1);
+		assertTrue("Lista deve ter o cliente inserido", clientes.get(0).getCodigo() == clienteCadastrado.getCodigo());
+	}
+	
+	/** TESTES EXISTS */
+	
+	public void testExistsIdsInvalidosNormalBehavior() {		
+		builder.criarClienteValido().setCodigo(-5L).assertNotExists();
+		builder.criarClienteValido().setCodigo(0L).assertNotExists();
+		builder.criarClienteValido().setCodigo(2L).assertNotExists();
+	}
+	
+	public void testExistsIdsValidosNormalBehavior() throws DataException, ClienteInvalidoException {	
+		builder.criarClienteValido().cadastrarCliente().assertExists();
+	}
+
+}
